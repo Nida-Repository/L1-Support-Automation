40712227499d24bdc9967906c7121e7a72a22039
Nida-Repository <nida@cloudetel.com>
2026-07-25 15:27:33 +0530

    added webhook.py to test prtg_alert

--- api/webhook.py
+++ api/webhook.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 from models.prtg_alert import PRTGWebhookPayload
 
 app = FastAPI()
-
+ #To test prtg_alert.py
 @app.post("/webhooks/prtg")
 async def receive_prtg_webhook(payload: PRTGWebhookPayload):
-    # handle payload here (e.g., push to Celery task, since you have celery installed)
+   
     return {"received": True, "sensor": payload.sensor_name, "status": payload.status}
